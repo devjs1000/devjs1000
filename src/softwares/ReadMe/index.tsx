27534f79529d61815f2b73c42d5519528c21fd0a
anand pandit <atim4077@gmail.com>
2022-12-17 00:41:08 +0530

    readme added

--- src/softwares/ReadMe/index.tsx
+++ src/softwares/ReadMe/index.tsx
@@ -1,27 +1,12 @@
 import React, { useEffect } from "react";
 import axios from "axios";
-import ReactMarkdown from "react-markdown";
-import gfm from "remark-gfm";
-import { Box, Spinner } from "@chakra-ui/react";
-import parse from "html-react-parser";
+import { Box, Flex, Heading, Spinner } from "@chakra-ui/react";
+import ReadMeJsx from "./ReadMeJsx";
 
 const ReadMe = () => {
-  const [data, setData] = React.useState(null);
-  const fetchData = async () => {
-    const result = await axios.get(
-      "https://raw.githubusercontent.com/devjs1000/devjs1000/master/README.md"
-    );
-    console.log(data);
-    setData(result.data);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  if (!data) return <Spinner />;
-
-  return <Box w={"100vw"}>{parse(data)}</Box>;
+  return <Box p={4}>
+    <ReadMeJsx />
+  </Box>;
 };
 
 export default ReadMe;
